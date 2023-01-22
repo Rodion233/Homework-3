7947a10f821fd038fb0fe383ad46f0a652f292f8
Rodion23 <rodion23@mail.ru>
2023-01-22 12:38:20 +0700

    Add HW23

--- HW23/Program.cs
+++ HW23/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int x1 = Prompt("Введите число N");
+int index = 1;
+while (index<=x1)
+{
+    double r = Math.Pow(index, 3);
+    System.Console.WriteLine(r);
+    index++;
+}
